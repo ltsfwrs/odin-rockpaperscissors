698eef1e54e3fce35c093d64925de7cf46d7b171
Cyber Sepp <sgmail1000@t-online.de>
2022-04-30 23:19:08 +0200

    add ui

--- script.js
+++ script.js
@@ -52,8 +52,79 @@ function playRound(playerSelection, computerSelection) {
         return [message, outcome]
     }
 }
+const resultDiv = document.querySelector('#result')
+const matchResultDiv = document.querySelector('#match-result')
+const runningScoreDiv = document.querySelector('#running-score')
 
-function Game() {
+const buttons = document.querySelectorAll('button')
+let roundsPlayed = 0
+let gamesWonByPlayer = 0
+let gamesWonByComputer = 0
+let gameResult = ""
+let winningMessage = false
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        roundsPlayed++
+        if (winningMessage === true) {
+            matchResultDiv.textContent = ""
+        }
+        gameResult = playRound(button.id, computerPlay())
+        resultDiv.textContent = gameResult[0]
+        playerResult = gameResult[1]
+
+        if (playerResult === "won") {
+            gamesWonByPlayer++
+            resultDiv.style.color = "green"
+        }
+        else if (playerResult === "lost") {
+            gamesWonByComputer++
+            resultDiv.style.color = "red"
+        }
+        else if (playerResult === "tie") {
+            resultDiv.style.color = "black"
+        }
+        runningScoreDiv.textContent = "Player: " + gamesWonByPlayer + " Computer: " + gamesWonByComputer
+
+        if (roundsPlayed === 5) {
+            declareWinner()
+            roundsPlayed = 0
+        }
+     })
+})
+
+function declareWinner() {
+    if (gamesWonByPlayer > gamesWonByComputer) {
+        matchResultDiv.textContent = "Player wins the match!"
+        matchResultDiv.style.color = "green"
+    }
+    else if (gamesWonByPlayer === gamesWonByComputer) {
+        matchResultDiv.textContent = "The match is a draw!"
+        matchResultDiv.style.color = "black"
+    }
+    else if (gamesWonByPlayer < gamesWonByComputer) {
+        matchResultDiv.textContent = "Computer wins the match!"
+        matchResultDiv.style.color = "red"
+    }
+    gamesWonByPlayer = 0
+    gamesWonByComputer = 0
+    gameResult = ""
+    winningMessage = true
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Play 5 Rounds
+/* function Game() {
     let gamesWonByPlayer = 0
     let gamesWonByComputer = 0
     for (let i = 0; i < 5; i++) {
@@ -77,5 +148,5 @@ function Game() {
         console.log("Computer wins the match!")
     }
 }
-
-Game();
+ */
+//Game();
